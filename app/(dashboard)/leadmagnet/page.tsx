3b32b7297ae51640cde9d756d77526a7d197d46e
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-01-23 23:57:18 +0530

    1

--- app/(dashboard)/leadmagnet/page.tsx
+++ app/(dashboard)/leadmagnet/page.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
-// import {auth} from '@clerck/nextjs'
+import {auth} from '@clerck/nextjs'
 
-// function LeadMagnetsPage() {
-//     const 
-// }
+function LeadMagnetsPage() {
+    const {userId} =auth ()
+    
+    if (!userId) return <div>No User Found...</div>
 
-const LeadMagnetsPage = () => {
-  return (
-    <div>LeadMagnetsPage</div>
-  )
-}
+const leadMagnetsRequest = getLeadMagnets(userId)
+const leadsRequest = getLeads (userId);
+
+const [leadMagnets,leads] =await Promise.all([
+leadMagnetsRequest,
+leadsRequest
+]),
+return <div>IrisLeadMagnetsPage</div>
+
+
 
 export default LeadMagnetsPage
